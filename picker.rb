9864b9b66c7375719f1c493db820844ab03d1d2f
Pongsathorn Antarikanon <s5311563208@gmail.com>
2014-10-31 00:11:39 +0700

    update picker

--- picker.rb
+++ picker.rb
@@ -1,1 +1,19 @@
-a
+def pick_engineer()
+	engineers = Array.new()
+	engineers = File.readlines("Engineer.txt")
+	name = engineers[rand(engineers.count)]
+	puts name
+	return name
+end
+
+pick_engineer()
+
+def featureA()
+end
+
+def featureB(pick_engineer)
+end
+
+def featureC()
+
+end
